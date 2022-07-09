4f6a40e8c3708bc3f48645580b9e3a98c7782bfd
dorme <d.orme@imperial.ac.uk>
2022-07-09 21:25:23 +0100

    Added include_coords function to coordsmod.f90 and renamed nearest to nearest_coords

--- coordsmod.f90
+++ coordsmod.f90
@@ -6,7 +6,8 @@ implicit none
 
 public  :: parsecoords
 public  :: calcpixels
-private :: nearest
+private :: nearest_coord
+private :: include_coord
 
 contains
 
@@ -69,10 +70,16 @@ integer :: endy
 
 !-------------------------
 
-srtx = nearest(bb(1),lon)
-srty = nearest(bb(2),lat)
-endx = nearest(bb(3),lon)
-endx = nearest(bb(4),lat)
+
+! srtx = nearest_coord(bb(1),lon)
+! srty = nearest_coord(bb(2),lat)
+! endx = nearest_coord(bb(3),lon)
+! endy = nearest_coord(bb(4),lat)
+
+srtx = include_coord(bb(1),lon, .true.)
+srty = include_coord(bb(2),lat, .true.)
+endx = include_coord(bb(3),lon, .false.)
+endy = include_coord(bb(4),lat, .false.)
 
 cntx = max(endx - srtx,1)
 cnty = max(endy - srty,1)
@@ -81,7 +88,7 @@ end subroutine calcpixels
 
 !-----------------------------------------------------------------------------------------------
 
-integer function nearest(val,vect)
+integer function nearest_coord(val,vect)
 
 real(dp) :: val
 real(dp), dimension(:) :: vect
@@ -90,9 +97,42 @@ integer, dimension(1) :: pos
 
 pos = minloc(abs(val-vect))
 
-nearest = pos(1)
+nearest_coord = pos(1)
 
-end function nearest
+end function nearest_coord
+
+!-----------------------------------------------------------------------------------------------
+
+integer function include_coord(val,vect,below)
+
+real(dp) :: val
+real(dp), dimension(:) :: vect
+logical :: below
+
+integer :: pos
+integer :: size_vect
+
+size_vect = size(vect)
+
+if (below) then
+  do pos = 1, size_vect
+    if (vect(pos).ge.val) then
+      include_coord = max(pos - 1, 1)
+      exit
+    end if
+  end do
+else
+  do pos = size(vect), 1, -1
+    if (vect(pos).le.val) then
+      include_coord = min(pos + 1, size_vect) 
+      exit
+    end if
+  end do
+end if
+
+print*, 'pos = ', pos
+
+end function include_coord
 
 !-----------------------------------------------------------------------------------------------
 
